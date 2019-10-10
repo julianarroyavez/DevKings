b1e34be7148ef2e30ebfc7cf426cb12cc9ade570
julianarroyavez <julianarroyavez@hotmail.com>
2019-10-10 19:41:21 -0400

    first commit

---
+++ DevSpadeGame/bord.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DevSpadeGame
+{
+    public partial class bord : Form
+    {
+        public bord()
+        {
+            InitializeComponent();
+        }
+
+        private void Bord_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
